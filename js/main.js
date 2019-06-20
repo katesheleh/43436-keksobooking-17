8a0ce422ea12140acf2da808164dbf72e6f2eef7
Kate Sheleh <kate.sheleh@gmail.com>
2019-06-20 19:48:56 +0200

    form validation

--- js/main.js
+++ js/main.js
@@ -4,14 +4,22 @@ function getRandomNum(min, max) {
 
 var mapForUsers = document.querySelector('.map__pins');
 
-var APPARTAMENT_TYPES = ["palace", "flat", "house", "bungalo"];
+var APPARTAMENT_TYPES = ["palace", "house", "flat", "bungalo"];
+var APPARTAMENT_PRICES = [10000, 5000, 1000, 0];
 var COUNT_MOCK_ELEMENTS = 8;
-
 var appartments = [];
-
 var similarListElement = document.querySelector('.map__pins');
-
 var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var mapwrapFaded = document.querySelector('.map--faded');
+var mapwrap = document.querySelector('.map');
+var mainpin = document.querySelector('.map__pin--main');
+var addressInput = document.querySelector('#address');
+var mainForm = document.querySelector('.ad-form');
+var mainFormDisabled = document.querySelector('.ad-form--disabled');
+var formElements = mainForm.querySelectorAll('input, select, textarea, button');
+var typeLivingSelect = mainForm.querySelector('#type');
+var priceInput = mainForm.querySelector('#price');
+var timeSelectWrapper = mainForm.querySelector('.ad-form__element--time');
 
 
 var addMockElement = function (arr, count, type, x, y) {
@@ -60,55 +68,41 @@ similarListElement.appendChild(fragment);
 
 document.querySelector('.map').classList.add('map--faded');
 
-var mainForm = document.querySelector('.ad-form');
-var mainFormDisabled = document.querySelector('.ad-form--disabled');
-
-var inputElements = mainForm.querySelectorAll('input');
-var selectElements = mainForm.querySelectorAll('select');
-var textareaElements = mainForm.querySelectorAll('textarea');
-var btnElements = mainForm.querySelectorAll('button');
-
 function setFieldStatus(element, status) {
     for (var i = 0; i < element.length; i++) {
         element[i].disabled = status;
     }
+    return true;
 }
 
-setFieldStatus(inputElements, true);
-setFieldStatus(selectElements, true);
-setFieldStatus(textareaElements, true);
-setFieldStatus(btnElements, true);
+setFieldStatus(formElements, true);
 
-var inputActive = setFieldStatus(inputElements, false);
-var selectActive = setFieldStatus(selectElements, false);
-var textareaActive = setFieldStatus(textareaElements, false);
-var btnActive = setFieldStatus(btnElements, false);
-
-
-var mapwrapFaded = document.querySelector('.map--faded');
-var mapwrap = document.querySelector('.map');
-var mainpin = document.querySelector('.map__pin--main');
-var addressInput = document.querySelector('#address');
-
-mainpin.addEventListener('click', function () {
+var activationMapFormHandler = function () {
     mapwrap.classList.remove('map--faded');
     mainForm.classList.remove('ad-form--disabled');
-    inputActive;
-    selectActive;
-    textareaActive;
-    btnActive;
+    setFieldStatus(formElements, false);
     addressInput.value = mainpin.style.left.replace('px', '') + ',' + mainpin.style.top.replace('px', '');
-});
+};
 
+mainpin.addEventListener('click', activationMapFormHandler);
 mainpin.addEventListener('mouseup', function (evt) {
     if (evt.keyCode === 13) {
-        mapwrap.classList.remove('map--faded');
-        mainForm.classList.remove('ad-form--disabled');
-        inputActive;
-        selectActive;
-        textareaActive;
-        btnActive;
-        addressInput.value = mainpin.style.top + ',' + mainpin.style.top;
-        addressInput.value = mainpin.style.left.replace('px', '') + ',' + mainpin.style.top.replace('px', '');
+        activationMapFormHandler;
     }
 });
+
+typeLivingSelect.addEventListener('change', function (evt) {
+    var selectedOption = APPARTAMENT_TYPES.indexOf(evt.target.value);
+    priceInput.min = APPARTAMENT_TYPES[selectedOption];
+    priceInput.placeholder = APPARTAMENT_PRICES[selectedOption];
+});
+
+timeSelectWrapper.addEventListener('change', function (evt) {
+    if (evt.target === timeSelectWrapper.elements[0]) {
+        timeSelectWrapper.elements[1].value = timeSelectWrapper.elements[0].value;
+    } else {
+        timeSelectWrapper.elements[0].value = timeSelectWrapper.elements[1].value;
+    }
+});
+
+console.log(timeSelectWrapper.elements);
